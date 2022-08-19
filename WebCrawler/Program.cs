909afacff13d4b4afc544eeb4ae206a60c92f015
Adel Kouaou <adel.kouaou@hotmail.com>
2022-08-19 01:09:42 -0400

    Added some comments and some refactoring

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -17,14 +17,15 @@ namespace WebCrawler
         private static Queue<Page> queue = new Queue<Page>();
         private static HashSet<Page> visitedWebsite = new HashSet<Page>();
 
-
         private static List<string> emails = new List<string>();
         private static HtmlWeb web = new HtmlWeb();
 
         static void Main(string[] args)
         {
 
-           // Directory.CreateDirectory(args[2]);
+
+            //TODO: Make a function to execute all follow verification to make the code cleaner.
+            // Directory.CreateDirectory(args[2]);
             if (args.Length != 3)
             {
                 Console.WriteLine("Veuillez entrer 3 arguments");
@@ -56,8 +57,9 @@ namespace WebCrawler
                 }
                 else
                 {
-                    Page page_init = new Page(args[1], Int32.Parse(args[0]));
-                    discoverWeb(page_init, args[2]);
+                    MessageValidation();
+                    Page intialPage = new Page(args[1], Int32.Parse(args[0]));
+                    discoverWeb(intialPage, args[2]);
 
                 }
 
@@ -73,9 +75,7 @@ namespace WebCrawler
         // Main functions
         private static void discoverWeb(Page rootURL, string Path)
         {
-            Page pageCourante = null;
-
-
+            Page currentePage = null;
 
 
 
@@ -87,54 +87,57 @@ namespace WebCrawler
                 try
                 {
 
-                    pageCourante = queue.Dequeue();
+                    currentePage = queue.Dequeue();
 
                     // get the name of the file in advance. ex  https://departement-info-cem.github.io/3N5-Prog3/testbot/index.html  ==>  index.html
-                    string fileName = getNameOfFile(pageCourante);
+                    string fileName = getNameOfFile(currentePage);
 
 
 
                     // connect to url
-                    var htmlDoc = web.Load(pageCourante.url);
-                    Console.WriteLine($"Exploration de  >>  {pageCourante.url}");
+                    var htmlDoc = web.Load(currentePage.url);
+                    Console.WriteLine($"Exploration de  >>  {currentePage.url}");
 
 
-                    var textModified = Emails(htmlDoc.Text, emails);
+                    // exctract emails of given html body , replace all emails by correc@ti.con 
+                    // and return that new modified html body text with email replaced by correc@ti.con
+                    // to indcate that email was found at that place
+                    var textModified = ExtractAndReplaceEmailsInText(htmlDoc.Text, emails);
 
 
 
 
+                    //creer un fichier et ecrit le body du loaded seulement si le nom du fichier  n'est pas vide. 
+                    //create a file and writes the loaded body only if name of the filename isnt empty ( could use stringIsNullOrEmpty(fileName)
                     string full_path = ($"{Path}/{fileName}");
-                    //creer un fichier et ecrit le body du loaded seulement si le nom du fichier contient n'est pas vide. 
-                    if (fileName.Length > 0) using (StreamWriter objFichierAEcrire = new StreamWriter(full_path)) objFichierAEcrire.Write(textModified);
+                    if (!string.IsNullOrEmpty(fileName)) using (StreamWriter objFichierAEcrire = new StreamWriter(full_path)) objFichierAEcrire.Write(textModified);
 
-                    // exctract emails..
 
 
                     // extract absolute links
-                    if (pageCourante.depth != 0)
+                    if (currentePage.depth != 0)
                     {
                         var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
-                        if (htmlNodes!=null)
+                        if (htmlNodes != null)
                         {
-                              foreach (var node in htmlNodes)
-                        {
-
-                            string href = node.Attributes["href"].Value;
-                            string absURL = GetAbsoluteUrlString(pageCourante.url, href);
-                            Page new_lien = new Page(absURL, pageCourante.depth - 1);
-
-
-                            // only add if never visited
-                            if (!visitedWebsite.Contains(new_lien))
+                            foreach (var node in htmlNodes)
                             {
-                                visitedWebsite.Add(new_lien);
-                                queue.Enqueue(new_lien);
-                            }
 
+                                string href = node.Attributes["href"].Value;
+                                string absURL = GetAbsoluteUrlString(currentePage.url, href);
+                                Page newPage = new Page(absURL, currentePage.depth - 1);
+
+
+                                // only add if never visited
+                                if (!visitedWebsite.Contains(newPage))
+                                {
+                                    visitedWebsite.Add(newPage);
+                                    queue.Enqueue(newPage);
+                                }
+
+                            }
                         }
-                        }
-                      
+
                     }
 
 
@@ -149,13 +152,14 @@ namespace WebCrawler
 
 
                         case "WebException":
-                            Console.WriteLine($"Page inaccessible  {pageCourante.url}");
-                            visitedWebsite.Remove(pageCourante);
+                            //Unaccessible page
+                            Console.WriteLine($"Page inaccessible  {currentePage.url}");
+                            visitedWebsite.Remove(currentePage);
                             break;
                         case "HtmlWebException":
-                            Console.WriteLine($"URL mal formé {pageCourante.url}");
-                            visitedWebsite.Remove(pageCourante);
-
+                            //Mal formed URL
+                            Console.WriteLine($"URL mal formé {currentePage.url}");
+                            visitedWebsite.Remove(currentePage);
                             break;
                         default:
                             break;
@@ -174,7 +178,7 @@ namespace WebCrawler
 
         }
 
-        public static string Emails(string text, List<string> emails)
+        public static string ExtractAndReplaceEmailsInText(string text, List<string> emails)
         {
             const string MatchEmailPattern =
               @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
@@ -211,13 +215,6 @@ namespace WebCrawler
             string name = URL.url.Split('/')[URL.url.Split('/').Length - 1];
             return name;
 
-
-        }
-        private static void createFile(string Path, string fileName)
-        {
-
-
-            File.Create($"{Path}/{fileName}");
 
         }
         private static string GetAbsoluteUrlString(string baseUrl, string url)
@@ -292,7 +289,9 @@ namespace WebCrawler
             emails.Sort();
             Console.WriteLine();
             Console.WriteLine();
+            //Number of explored pages
             Console.WriteLine($"Nombre de pages explorées : {visitedWebsite.Count}");
+            //Number of extracted emails (ordered asc)
             Console.WriteLine($"Nombre de courriels extraits (en ordre alphabétique) : {emails.Count}");
             foreach (var email in emails)
             {
---
+++ WebCrawler/bin/Debug/temp/index.php
@@ -0,0 +1,158 @@
+<p align="center"><b>WARNING:</b> This pages is using the global variables.php file. All 240 course websites after Fall 2018 should use a relative path to a variables.php file in the semester's root directory instead!</p><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Downloads - CS 240 - Fall 2018</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- styles -->
+    <link href="https://students.cs.byu.edu/~cs240ta/fall2018/include/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="https://students.cs.byu.edu/~cs240ta/fall2018/include/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="https://students.cs.byu.edu/~cs240ta/fall2018/include/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="https://students.cs.byu.edu/~cs240ta/fall2018/">CS 240</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/projects/">Projects</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Schedule <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/info/schedule.php">Class Schedule</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/TA/">TA Schedule</a></li>
+                </ul>
+              </li>
+
+              <!-- You can use this example for how to make drop-down menus or to add
+              		 additional pages to an existing drop-down menu. -->
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Policies <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/info/policies.php">Class Policies</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/info/TA_help_policies.php">TA Help Policies</a></li>
+
+                  <!-- Below is an example of how to add an item to an existing
+                  	  drop-down menu.  Just create a list-item tag and a
+                  	  reference tag and you're good to go...
+                  	 <li><a href="http://www.websiteaddress.com/path/to/your/domain">LINK ONE</a></li>
+                  	 		or
+                  	 <li><a href="path/from/our/directory/system">LINK TWO</a></li> -->
+
+                </ul>
+              </li>
+
+		<li class="active" ><a href="https://students.cs.byu.edu/~cs240ta/fall2018/download/">Download</a></li>
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Lecture Files <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/rodham_files/">Dr. Rodham</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/powerpoints/">Dr. Woodfield</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/jones_files/">Dr. Jones</a></li>
+                  <li><a href="https://faculty.cs.byu.edu/~barker/cs240/notes/">Dr. Barker</a></li>
+                  <!--<li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/tyler_monson_files/">Tyler Monson</a></li>-->
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Help <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+				  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/faqs/">FAQs</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/help/">Project Tips</a></li>
+                  <li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/tutorials/">Tutorials</a></li>
+                  <li><a href="http://docs.oracle.com/javase/8/docs/api/">Java API</a></li>
+                  <!--<li><a href="http://docs.oracle.com/javase/8/docs/">Java Documentation</a></li>-->
+				  <li><a href="https://docs.oracle.com/javase/8/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/package-summary.html">HttpServer Documentation</a></li>
+                  <li><a href="http://www.sqlite.org/lang.html">SQL Support in SQLite</a></li>
+                </ul>
+              </li>
+		<li><a href="https://students.cs.byu.edu/~cs240ta/fall2018/contact/">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container maincontainer">
+
+			<div class="box">
+		<h1>Downloads</h1>
+		</div>
+
+		<div class="main">
+	
+    <!--End header.  Close div, body, and html in footer -->
+
+    <div class="center">
+      <h2>CS 240 Downloads </h2>
+    </div>
+    
+
+<hr />
+
+<!--<img src="/icons/back.gif" alt="[DIR]" /> <a href="?path=.%2F">Parent Directory</a><br />-->
+
+
+<img src="/icons/blank.gif" alt="[   ]"> <a href="AsyncWebAccess.tgz">
+AsyncWebAccess.tgz</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="AsyncWebAccess.zip">
+AsyncWebAccess.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="BookClub.tgz">
+BookClub.tgz</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="BookClub.zip">
+BookClub.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="CatalogDemo.tgz">
+CatalogDemo.tgz</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="CatalogDemo.zip">
+CatalogDemo.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="FamilyMapProject.zip">
+FamilyMapProject.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="IconifyExample-Deprecated.zip">
+IconifyExample-Deprecated.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="WidgetDemo2.tar.gz">
+WidgetDemo2.tar.gz</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="WidgetDemo2.zip">
+WidgetDemo2.zip</a><br />
+</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="javadocExamplesFor240.zip">
+javadocExamplesFor240.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="progressiveClientServerExamples.zip">
+progressiveClientServerExamples.zip</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="sqlite.tgz">
+sqlite.tgz</a><br />
+<img src="/icons/blank.gif" alt="[   ]"> <a href="sqlite.zip">
+sqlite.zip</a><br />
+
+			</div>
+	
+     <footer>
+        <p>&copy; BYU 2018</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- ================================================== -->
+    <!-- Javascript - Placed at the end of the document so the page appears to load faster -->
+    <script src="https://students.cs.byu.edu/~cs240ta/fall2018/include/jquery/jquery-1.10.2.min.js"></script>
+    <script src="https://students.cs.byu.edu/~cs240ta/fall2018/include/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
